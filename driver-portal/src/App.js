9293eaf084479d311c14f8e6aac3d31253e9805b
Hansadee Rathnayaka <hansadirathnayaka@gmail.com>
2026-06-13 12:31:12 +0530

    feat: Add the background image for the HomePage

--- driver-portal/src/App.js
+++ driver-portal/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './components/Common/Header';
 import Footer from './components/Common/Footer';
@@ -8,21 +8,30 @@ import DetailsPage from './pages/DetailsPage';
 import PaymentPage from './pages/PaymentPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 
+function AppLayout() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="App">
+      <Header />
+      <main className={`main-content ${isHomePage ? 'main-content-home' : ''}`}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/details" element={<DetailsPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/details" element={<DetailsPage />} />
-            <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/confirmation" element={<ConfirmationPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppLayout />
     </Router>
   );
 }
