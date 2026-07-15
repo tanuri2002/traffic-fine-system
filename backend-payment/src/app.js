21927b379b4d18bc28ee80834d9085b625adb00e
sub-stack-dot <subhanisenevirathne9@gmail.com>
2026-07-16 03:25:01 +0530

    pushing unstaged files

--- backend-payment/src/app.js
+++ backend-payment/src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 const paymentRoutes = require('./routes/paymentRoutes');
 const statsRoutes = require('./routes/statsRoutes');
+
 
 app.use('/api/payment', paymentRoutes);
 app.use('/api/stats', statsRoutes);
